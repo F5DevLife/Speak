d39efbfe7b8fef8fe2693761f9aca0f7ac698f23
Ryan Hawkins <rkhawkins80@hotmail.com>
2021-12-23 20:08:06 +0000

    UX - Added more functionality to the controls.

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 const $ = (q) => document.querySelector(q)
 const text = $("#text")
 
+const rateSelection = $("#rate")
 const genderSelection = $("#gender")
-
-
-const resetButton = $("#reset")
-
+const pitchSelection = $("#pitch")
 
 const voiceSelection = $("#voice")
 
+const resetButton = $("#reset")
 
 const femaleVoiceIndexes = [2, 3, 4, 5, 7, 8, 9]
+const maleVoiceIndexes = [1, 6]
 
 
 $("#start").addEventListener("click", sayIt)
-resetButton.addEventListener("click", resetText)
+resetButton.addEventListener("click", resetSettings)
 window.addEventListener("keydown", function (event) {
     if (event.keyCode == "13") { 
         sayIt()
-        resetText()
+        resetSettings()
         }
 })
 
 genderSelection.addEventListener("change", function() {
+    updateVoiceIndexes(genderSelection.value)
     console.log(`Changed voice gender to ${genderSelection.value}.`);
 })
 
-function resetText() {
+function resetSettings() {
     text.value = ""
     text.focus()
+    rateSelection.value = 1
+    pitchSelection.value = 1
 }
 
 //Used for talking in the console
@@ -37,13 +40,34 @@ function talk(message) {
     speechSynthesis.speak(utterance)
 }
 
+function updateVoiceIndexes(gender) {
+    if (gender == "female") {
+        voiceSelection.value = 5
+        //Remove the male list indexes from the options.
+
+        //Set the lists of female indexes into the options.
+        
+    } 
+    else if (gender == "male") {
+        voiceSelection.value = 6
+        //Remove the female list indexes from the options.
+
+        //Set the lists of male indexes into the options.
+
+    } 
+    else {
+
+    }
+    console.log(`Updated voice gender listings to ${gender}`)
+}
+
 function sayIt() {
-    //const femVoiceIndex
     const message = text.value
-    const voiceNum = parseInt(voiceSelection.value)
+    const voiceIndex = parseInt(voiceSelection.value)
     const utterance = new SpeechSynthesisUtterance(message);
-    utterance.rate = 1
-    utterance.voice = speechSynthesis.getVoices()[voiceNum]
+    utterance.rate = rateSelection.value
+    utterance.pitch = pitchSelection.value
+    utterance.voice = speechSynthesis.getVoices()[voiceIndex]
     speechSynthesis.speak(utterance)
     console.log(`Said:  ${message}`)
 }
